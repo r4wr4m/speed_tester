addef60d53e1ea92f5bdd9b12817ee8c7f597ce8
r4wr4m <61500192+r4wr4m@users.noreply.github.com>
2025-05-05 14:34:56 +0200

    Update drawplot.py
    added average speeds

--- drawplot.py
+++ drawplot.py
@@ -16,7 +16,16 @@ start_time=[]
 start_time_str=[]
 stop_time=[]
 test_duration=[]
+average_down=[]
+average_up=[]
 
+def calculate_avg(values,average):
+    if len(values)-len(average)!=0:
+        avg=sum(values[len(average):len(values)])/(len(values)-len(average))
+        for i in range(len(average),len(values)):
+            average.append(avg)
+    return average
+    
 with open(result_file) as f:
     reader = csv.reader(f)
     for row in reader:
@@ -35,11 +44,22 @@ with open(result_file) as f:
             plt.axvline(x,linestyle='dotted',color="red")
             plt.text(x+1,expected_speeds[0][0]/4*3,row[0][3:],color="red",rotation=90)
             
+            #Calculating average
+            if len(download)>0:
+                average_down = calculate_avg(download,average_down)
+                average_up = calculate_avg(upload,average_up)
+average_down = calculate_avg(download,average_down)
+average_up = calculate_avg(upload,average_up)
+
 plt.plot(start_time_str, download, label="Download [Mbps]")
 plt.plot(start_time_str, upload, label="Upload [Mbps]")
 
 for expected_speed in expected_speeds: #Drawing expected speeds
     plt.plot(start_time_str, [expected_speed[0]]*len(download), label=expected_speed[1],linestyle='dashed')
+
+plt.plot(start_time_str, average_down, label='Average download speed',linestyle='dashed')#,color='yellow')
+plt.plot(start_time_str, average_up, label='Average upload speed',linestyle='dashed')#,color='yellow')
+
 
 #Latency, test duration
 #plt.bar(start_time_str, test_duration, label="Test duration [s]")
